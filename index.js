9eb5e289347fc151e1e705c4f4c3cf8dfa9cef2f
rajeshharidas <rharidas18@gmail.com>
2017-05-21 23:52:18 -0500

    sdfs

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ restService.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     
 	request.get(
-			"https://ps.pndsn.com/publish/pub-c-7e402782-cf0c-44b9-a1ea-1fc85de8950b/sub-c-daf1bbd2-3d74-11e7-b6fd-02ee2ddab7fe/0/Channel-yctqeal7n/myCallback/%7B%22text%22%3A%22lightson%22%7D?store=0&uuid=db9c5e39-7c95-40f5-8d71-125765b6f561",
+			"https://ps.pndsn.com/publish/pub-c-7e402782-cf0c-44b9-a1ea-1fc85de8950b/sub-c-daf1bbd2-3d74-11e7-b6fd-02ee2ddab7fe/0/Channel-ekigfo5xv/myCallback/%7B%22text%22%3A%22lightson%22%7D?store=0&uuid=db9c5e39-7c95-40f5-8d71-125765b6f561",
 			function(error, res2, body) {
 				if (!error && res2.statusCode == 200) {
 					console.log(body)

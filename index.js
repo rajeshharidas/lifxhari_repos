a559fdf17df42b38fb2d49551e0e9f06d4ed395e
rajeshharidas <rharidas18@gmail.com>
2017-05-22 12:48:08 -0500

    bcbcv

--- index.js
+++ index.js
@@ -35,6 +35,26 @@ restService.post('/turnlightson', function(req, res) {
     });
 });
 
+restService.post('/turnlightsoff', function(req, res) {
+    var speech = "Lights are turned off now.";
+    
+	request.get(
+			"https://ps.pndsn.com/publish/pub-c-7e402782-cf0c-44b9-a1ea-1fc85de8950b/sub-c-daf1bbd2-3d74-11e7-b6fd-02ee2ddab7fe/0/Channel-ekigfo5xv/myCallback/%7B%22text%22%3A%22lightsoff%22%7D?store=0&uuid=db9c5e39-7c95-40f5-8d71-125765b6f561",
+			function(error, res2, body) {
+				if (!error && res2.statusCode == 200) {
+					console.log(body)
+				} else {
+					console.log('Error:' + error + 'Body: ' + body);
+				}
+			}
+		);
+	
+	return res.json({
+        speech: speech,
+        displayText: speech,
+        source: 'LIFXHari'
+    });
+});
 
 
 restService.listen((process.env.PORT || 8000), function() {

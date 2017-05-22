bdb3688691bf6c9d07443ebc6d2ded076fa7917c
rajeshharidas <rharidas18@gmail.com>
2017-05-22 10:47:28 -0500

    sasdas

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
-// Copyright 2016, Google, Inc.
-// Licensed under the Apache License, Version 2.0 (the 'License');
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an 'AS IS' BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 'use strict';
-
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const path = require('path');
 const restService = express();
 
 restService.use(bodyParser.urlencoded({
@@ -25,6 +11,9 @@ restService.use(bodyParser.urlencoded({
 
 restService.use(bodyParser.json());
 var request = require('request');
+
+restService.use(express.static(path.join(__dirname, 'public')));
+
 restService.post('/turnlightson', function(req, res) {
     var speech = "Lights are turned on now.";
     
